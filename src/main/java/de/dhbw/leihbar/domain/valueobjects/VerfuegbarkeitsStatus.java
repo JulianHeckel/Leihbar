a6ef4ae5fa51f0b276210379379e41fcb5fedf0c
Julian <julian.heckel@siemens-energy.com>
2026-03-20 21:15:00 +0100

    Erste Value Objects: Status-Enums
    VerfuegbarkeitsStatus und AusleiheStatus als Enums implementiert.
    Jeder Status hat eine Bezeichnung und Hilfsmethoden
    (istAusleihbar, istAktiv).

--- src/main/java/de/dhbw/leihbar/domain/valueobjects/VerfuegbarkeitsStatus.java
+++ src/main/java/de/dhbw/leihbar/domain/valueobjects/VerfuegbarkeitsStatus.java
@@ -1,30 +1,36 @@
 package de.dhbw.leihbar.domain.valueobjects;
 
 /**
- * Enum fuer den Verfuegbarkeitsstatus eines Gegenstandes.
- *
- * Geplante Werte:
- * - VERFUEGBAR:   Gegenstand kann ausgeliehen werden
- * - AUSGELIEHEN:  Gegenstand ist aktuell verliehen
- * - IN_WARTUNG:   Gegenstand temporaer gesperrt (Reparatur, Reinigung)
- * - AUSGEMUSTERT: Gegenstand endgueltig aus dem Bestand entfernt
- *
- * Jeder Status soll eine Bezeichnung (String) und einen
- * boolean istAusleihbar haben.
- *
- * Geplante Methoden:
- * - getBezeichnung(): String (menschenlesbare Bezeichnung)
- * - istAusleihbar(): boolean (nur VERFUEGBAR = true)
- * - toString(): gibt Bezeichnung zurueck
- *
- * Wird von Gegenstand.istVerfuegbar() genutzt.
- *
- * TODO: Enum-Konstanten mit Bezeichnung und ausleihbar-Flag
- * TODO: Konstruktor, Getter, toString
+ * Enum für den Verfügbarkeitsstatus eines Gegenstandes.
  */
 public enum VerfuegbarkeitsStatus {
-    // TODO: VERFUEGBAR("Verfuegbar", true),
-    // TODO: AUSGELIEHEN("Ausgeliehen", false),
-    // TODO: IN_WARTUNG("In Wartung", false),
-    // TODO: AUSGEMUSTERT("Ausgemustert", false);
+
+    VERFUEGBAR("Verfügbar", true),
+    AUSGELIEHEN("Ausgeliehen", false),
+    IN_WARTUNG("In Wartung", false),
+    AUSGEMUSTERT("Ausgemustert", false);
+
+    private final String bezeichnung;
+    private final boolean ausleihbar;
+
+    VerfuegbarkeitsStatus(String bezeichnung, boolean ausleihbar) {
+        this.bezeichnung = bezeichnung;
+        this.ausleihbar = ausleihbar;
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    /**
+     * Gibt an, ob ein Gegenstand mit diesem Status ausgeliehen werden kann.
+     */
+    public boolean istAusleihbar() {
+        return ausleihbar;
+    }
+
+    @Override
+    public String toString() {
+        return bezeichnung;
+    }
 }
